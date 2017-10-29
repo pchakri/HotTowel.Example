31bff09841a6da080b8f3a8494e8ba440f62ff0b
pchakri <pbchakravarthi@gmail.com>
2017-10-29 16:41:52 -0400

    Initial commit

---
+++ HotTowel.Example/app/monitor/monitorCtrl.js
@@ -0,0 +1,20 @@
+﻿(function () {
+    'use strict';
+    var controllerId = 'monitor';
+    angular.module('app').controller(controllerId, ['common', monitor]);
+
+    function monitor(common) {
+        var getLogFn = common.logger.getLogFn;
+        var log = getLogFn(controllerId);
+
+        var vm = this;
+        vm.title = 'Monitor';
+
+        activate();
+
+        function activate() {
+            common.activateController([], controllerId)
+                .then(function () { log('Activated monitor View'); });
+        }
+    }
+})();
